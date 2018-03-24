703b88bbf138be51d090f5b4a0a45e47d8a10e8b
jorgegomesen <37339245+jorgegomesen@users.noreply.github.com>
2018-03-24 15:46:18 -0300

    Add files via upload
    Algumas modificações realizadas no algoritmo, visto que o algoritmo presente no livro não servia para nosso problema, o mesmo apresenta duas funções diferenciais que dependem de três variáveis diferentes.

--- rk4.c
+++ rk4.c
@@ -1,59 +1,56 @@
 #include <stdio.h>
-#include <math.h>
 
-// float rungeFunct(float, float);
-// float rungeKutta4(float, float, float, float);
+typedef struct hosp_Paras{
+  float g[2];
+  float d[2];
+  float h;
+  float t;
+  float tf;
+  double Par;
+  double Hosp;
+  double k[4];
+  double l[4];
+}HOSP_PARAS;
 
-float rungeFunct(float y, float d, float g, float Par, int param){
-    if (!param){
-        // printf("\ny -> %.4f -- (%.4f * %.4f) - (%.4f * %.4f * %.4f) = %.4f", y, g, y, d, Par, y, (g * y) - (d * Par * y));
-        return ((g * y) - (d * Par * y));
-    }else{
-        // printf("\ny -> %.4f -- -(%.4f * %.4f) + (%.4f * %.4f * %.4f) = %.4f", y, d, y, g, Par, y, -(d * y) + (g * Par * y));
-        return (-(d * y) + (g * Par * y));
-    }
+double rungeFunct(double, double, float, float);
+void HospParas(HOSP_PARAS *);
+
+double rungeFunct(double x, double y, float a, float b){
+    return ((a * x) + (b * x * y));
 }
 
-float rungeKutta4(float y, float h, float d, float g, float Par, int param){
-    float k1, k2, k3, k4;
-    k1 = rungeFunct(y, d, g, Par, param);
-    k2 = rungeFunct(y + (h/2) * k1, d, g, Par, param);
-    k3 = rungeFunct(y + (h/2) * k2, d, g, Par, param);
-    k4 = rungeFunct(y + h * k3, d, g, Par, param);
-    y += (h/6.0) * (k1 + 2.0*k2 + 2.0*k3 + k4);
-    return y;
-}
-
-float HospParas(float d1, float d2, float g1, float g2, float Hosp, float Par, float t, float tf, float h){
-    float aux;
-    for(; t <= 1; t+=h){
-        aux = rungeKutta4(Hosp, h, d1, g1, Par, 0);
-        Par = rungeKutta4(Par, h, d2, g2, Hosp, 1);
-        printf("\nHospedeiros -> %.4f ---- Pararasitas -> %.4f ", aux, Par);
-        Hosp = aux;
+void HospParasRungeKutta(HOSP_PARAS *hp){
+    double Hosp = hp->Hosp, Paras = hp->Par;
+    float t = hp->t, tf = hp->tf, h=hp->h;
+    for(; t <= tf; t += h){
+        printf("\nt-> %.1f -- Hospedeiros -> %.8lf ---- Pararasitas -> %.8lf ", t, Hosp, Paras);
+        hp->k[0] = h * rungeFunct(Hosp, Paras, hp->g[0], -(hp->d[0]));
+        hp->l[0] = h * rungeFunct(Paras, Hosp, -(hp->d[1]), hp->g[1]);
+        hp->k[1] = h * rungeFunct((Hosp + 0.5 * h * hp->k[0]), (Paras + 0.5 * h * hp->l[0]), hp->g[0], -(hp->d[0]));
+        hp->l[1] = h * rungeFunct((Paras + 0.5 * h * hp->l[0]), (Hosp + 0.5 * h * hp->k[0]), -(hp->d[1]), hp->g[1]);
+        hp->k[2] = h * rungeFunct((Hosp + 0.5 * h * hp->k[1]), (Paras + 0.5 * h * hp->l[1]), hp->g[0], -(hp->d[0]));
+        hp->l[2] = h * rungeFunct((Paras + 0.5 * h * hp->l[1]), (Hosp + 0.5 * h * hp->k[1]), -(hp->d[1]), hp->g[1]);
+        hp->k[3] = h * rungeFunct((Hosp + hp->k[2]), (Paras + hp->l[2]), hp->g[0], -(hp->d[0]));
+        hp->l[3] = h * rungeFunct((Paras + hp->l[2]), (Hosp + hp->k[2]), -(hp->d[1]), hp->g[1]);
+        Hosp += ((1 / 6.0) * (hp->k[0] + (2.0 * hp->k[1]) + (2.0 * hp->k[2]) + hp->k[3]));
+        Paras += ((1 / 6.0) * (hp->l[0] + (2.0 * hp->l[1]) + (2.0 * hp->l[2]) + hp->l[3]));
     }
+    hp->Hosp = Hosp;
+    hp->Par = Paras;
+    printf("\n\n");
 }
 
 int main(int argc, char** argv){
-    float Par = 50, Hosp = 20, g1 = 1, g2 = 0.3, d1 = 0.7, d2 = 0.5, h = 0.1, t = 0, tf = 35;
-    // printf("\n\nEntre com os Parasitas. ");
-    // scanf("%f", &Par);
-    // printf("\nEntre com os Hospedeiros. ");
-    // scanf("%f", &Hosp);
-    // printf("\nEntre com o g1. ");
-    // scanf("%f", &g1);
-    // printf("\nEntre com o g2. ");
-    // scanf("%f", &g2);
-    // printf("\nEntre com o d1. ");
-    // scanf("%f", &d1);
-    // printf("\nEntre com o d2. ");
-    // scanf("%f", &d2);
-    // printf("\nEntre com o h. ");
-    // scanf("%f", &h);
-    // printf("\nEntre com o Tempo inicial. ");
-    // scanf("%f", &t);
-    // printf("\nEntre com o Tempo final. ");
-    // scanf("%f", &tf);
-    printf("\n\nResultado: %.4f\n\n", HospParas(d1, d2, g1, g2, Hosp, Par, t, tf, h));
+    HOSP_PARAS hp;
+    hp.g[0] = 1.0;
+    hp.g[1] = 0.3;
+    hp.d[0] = 0.7;
+    hp.d[1] = 0.5;
+    hp.h = 0.1;
+    hp.t = 0.0;
+    hp.tf = 35.0;
+    hp.Par = 50.0;
+    hp.Hosp = 20.0;
+    HospParasRungeKutta(&hp);
     return 0;
 }

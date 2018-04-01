b539b242dfcfb7cf6ea36aa0e19889e29e6eaae0
jorgegomesen <37339245+jorgegomesen@users.noreply.github.com>
2018-04-01 10:31:05 -0300

    Add files via upload
    Alterações na ordem dos parâmetros adequando assim a qual função será chamada.

--- rk4.py
+++ rk4.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# dicionário contendo os parâmetros utilizados nas funções diferenciais
 hp = {
     "ht": 20.,
     "p": 50.,
@@ -16,7 +17,7 @@ hp = {
 
 # função para desenhar os gráficos dos hospedeiros e parasitas em função do tempo
 def draw_graph(x, y, xlabel, ylabel):
-    plt.title(xlabel +" x "+ ylabel)
+    plt.title(xlabel + " x " + ylabel)
     plt.plot(x, y, c='#FFCC00', lw=1, marker='o', ms=2, mec='b', mew=1)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -25,7 +26,6 @@ def draw_graph(x, y, xlabel, ylabel):
 
 # equações diferencias Lotka-Volterra
 def lv_eqt(x, y, a, b):
-    # return x * a + y * b
     return x * (a + b * y)
 
 
@@ -37,17 +37,28 @@ def rk4_host_paras(h_p):
     for i in np.arange(h_p["t"], h_p["tf"] + h_p["h"], h_p["h"]):
         print("t= %.2f \t %.5f \t\t\t\t %.5f" % (i, h_p["ht"], h_p["p"]))
 
+        # kn1 = f(tn, xn, yn)
+        # ln1 = g(tn, yn, xn)
+        # kn2 = f(tn + h / 2, xn + hkn1/2, yn + hln1/2)
+        # ln2 = g(tn + h / 2, yn + hln1/2, xn + hkn1/2)
+        # kn3 = f(tn + h / 2, xn + hkn2/2, yn + hln2/2)
+        # ln3 = g(tn + h / 2, yn + hln2/2, xn + hkn2/2)
+        # kn4 = f(tn + h, xn + hkn3, yn + hln3)
+        # ln4 = g(tn + h, yn + hln3, xn + hkn3)
+        # xn + 1 = xn + h(kn1 + 2kn2 + 2kn3 + kn4) / 6.0
+        # yn + 1 = yn + h(ln1 + 2ln2 + 2ln3 + ln4) / 6.0
+
         f[0] = lv_eqt(h_p["ht"], h_p["p"], h_p["g"][0], h_p["d"][0])
-        g[0] = lv_eqt(h_p["ht"], h_p["p"], h_p["d"][1], h_p["g"][1])
+        g[0] = lv_eqt(h_p["p"], h_p["ht"], h_p["d"][1], h_p["g"][1])
 
         f[1] = lv_eqt(h_p["ht"] + 0.5 * f[0] * h_p["h"], h_p["p"] + 0.5 * g[0] * h_p["h"], h_p["g"][0], h_p["d"][0])
-        g[1] = lv_eqt(h_p["ht"] + 0.5 * f[0] * h_p["h"], h_p["p"] + 0.5 * g[0] * h_p["h"], h_p["d"][1], h_p["g"][1])
+        g[1] = lv_eqt(h_p["p"] + 0.5 * g[0] * h_p["h"], h_p["ht"] + 0.5 * f[0] * h_p["h"], h_p["d"][1], h_p["g"][1])
 
         f[2] = lv_eqt(h_p["ht"] + 0.5 * f[1] * h_p["h"], h_p["p"] + 0.5 * g[1] * h_p["h"], h_p["g"][0], h_p["d"][0])
-        g[2] = lv_eqt(h_p["ht"] + 0.5 * f[1] * h_p["h"], h_p["p"] + 0.5 * g[1] * h_p["h"], h_p["d"][1], h_p["g"][1])
+        g[2] = lv_eqt(h_p["p"] + 0.5 * g[1] * h_p["h"], h_p["ht"] + 0.5 * f[1] * h_p["h"], h_p["d"][1], h_p["g"][1])
 
         f[3] = lv_eqt(h_p["ht"] + f[2] * h_p["h"], h_p["p"] + g[2] * h_p["h"], h_p["g"][0], h_p["d"][0])
-        g[3] = lv_eqt(h_p["ht"] + f[2] * h_p["h"], h_p["p"] + g[2] * h_p["h"], h_p["d"][1], h_p["g"][1])
+        g[3] = lv_eqt(h_p["p"] + g[2] * h_p["h"], h_p["ht"] + f[2] * h_p["h"], h_p["d"][1], h_p["g"][1])
 
         h_p["plotH"].append(h_p["ht"])
         h_p["plotP"].append(h_p["p"])
